7900522df0312798e49252a64e3e0849758be610
lapwat <quentin@lapw.at>
2023-12-29 00:04:50 +0100

    add README and cd command

---
+++ README.md
@@ -0,0 +1,41 @@
+# gitkit
+
+Manage your Git repositories
+
+## Installation
+
+```sh
+cargo install --path .
+```
+
+## Usage
+
+```
+Usage: gitkit [OPTIONS] <COMMAND>
+
+Commands:
+  add   Clone a git repository
+  cd    Generate a cd command to be executed in your shell
+  help  Print this message or the help of the given subcommand(s)
+
+Options:
+  -u, --user <USER>            GitHub username [env: USER=lapwat] [default: $USER]
+  -d, --directory <DIRECTORY>  Directory where your repositories are stored [env: DIRECTORY=] [default: ~/gitkit]
+  -s, --ssh-key <SSH_KEY>      SSH key used to clone repositories [env: SSH_KEY=] [default: ~/.ssh/id_ed25519]
+  -h, --help                   Print help
+  -V, --version                Print version
+```
+
+## `cd` command
+
+This program cannot change your current directory.
+
+The `gitkit cd` command generates a `cd` command to be executed by your shell.
+
+Add this function to your `.bashrc` / `.zshrc`:
+
+```sh
+function gkcd () {
+    $(gitkit cd $1)
+}
+```
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+use std::path::Path;
+use std::process::{Command as ProcessCommand};
+
 use clap::{Args, Parser, Subcommand};
-use git2::Repository;
+use git2::{Cred, RemoteCallbacks};
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -13,16 +16,27 @@ struct Arguments {
 
 #[derive(Args)]
 struct Global {
-    #[arg(short, long, env)]
+    /// GitHub username
+    #[arg(short, long, env, default_value = "$USER")]
     user: String,
 
+    /// Directory where your repositories are stored
     #[arg(short, long, env, default_value = "~/gitkit")]
-    directory: String
+    directory: String,
+
+    /// SSH key used to clone repositories 
+    #[arg(short, long, env, default_value = "~/.ssh/id_ed25519")]
+    ssh_key: String,
 }
 
 #[derive(Subcommand)]
 enum Command {
+    /// Clone a git repository
     Add {
+        repository: String,
+    },
+    /// Generate a cd command to be executed in your shell
+    Cd {
         repository: String,
     },
 }
@@ -30,8 +44,9 @@ enum Command {
 fn main() {
     let mut arguments = Arguments::parse();
 
-    // expand ~ in workdir
+    // expand ~ in arguments 
     arguments.global.directory = shellexpand::tilde(&arguments.global.directory).to_string();
+    arguments.global.ssh_key = shellexpand::tilde(&arguments.global.ssh_key).to_string();
    
     match arguments.command {
         Command::Add { repository } => {
@@ -43,18 +58,61 @@ fn main() {
             }
 
             // prepend domain to repo if needed  
-            if !url.contains("http://") && !url.contains("https://") {
-                url = format!("https://github.com/{}", url);
+
+            // SSH
+            if !url.contains("git@") {
+                 url = format!("git@github.com:{}", url);
             }
 
             let clone_directory = format!("{}/{}", arguments.global.directory, repository);
 
+            // HTTP
+            // if !url.contains("http://") && !url.contains("https://") {
+            //     url = format!("https://github.com/{}", url);
+            // }
+            // let repo = match Repository::clone(&url, clone_directory) {
+            //     Ok(repo) => repo,
+            //     Err(e) => panic!("failed to clone: {}", e),
+            // };
+
             println!("cloning... {} -> {}", url, clone_directory);
 
-            let repo = match Repository::clone(&url, clone_directory) {
-                Ok(repo) => repo,
-                Err(e) => panic!("failed to clone: {}", e),
-            };
+            // prepare callbacks
+            let mut callbacks = RemoteCallbacks::new();
+            callbacks.credentials(|_url, username_from_url, _allowed_types| {
+                Cred::ssh_key(
+                    username_from_url.unwrap(),
+                    None,
+                    Path::new(&arguments.global.ssh_key),
+                    None,
+                )
+            });
+
+            // prepare fetch options
+            let mut fo = git2::FetchOptions::new();
+            fo.remote_callbacks(callbacks);
+
+            // prepare builder
+            let mut builder = git2::build::RepoBuilder::new();
+            builder.fetch_options(fo);
+
+            // clone the project
+            match builder.clone(
+              &url,
+              Path::new(&clone_directory),
+            ) {
+                Ok(_) => println!("ok"),
+                Err(e) => panic!("{}", e),
+            }
+        },
+        Command::Cd { repository } => {
+            let command = format!("cd {}/{}", arguments.global.directory, repository);
+            println!("{}", command);
+
+            ProcessCommand::new("sh")
+                .arg(command)
+                .output()
+                .expect("failed to change directory");
         },
     }
 }
